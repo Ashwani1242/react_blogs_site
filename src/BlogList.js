33da5d77224163ecbfc8af4f2d526730da874d8b
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-04-12 16:06:56 +0530

    Changes

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,15 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
 
     return (
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h1>{blog.title}</h1>
-                    <h5>Written by - {blog.author}</h5>
+                    <div>
+                        <h1>{blog.title}</h1>
+                        <h5>Written by - {blog.author}</h5>
+                    </div>
+                    <button onClick={() => handleDelete(blog.id)}>Delete</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -7,14 +7,19 @@ const Home = () => {
         { title: 'My new Fuvking website!', body: 'lorem ipsum...', author: 'Bobbombom', id: 1 },
         { title: 'A Fuvking welcome party!', body: 'lorem ipsum...', author: 'Bhupendra Jogi', id: 2 },
         { title: 'Top Fuvking Web Dev Tips', body: 'lorem ipsum...', author: 'Gabriella', id: 3 },
-        { title: 'How to become a Fuvking Developer', body: 'lorem ipsum...', author: 'Rasputin', id: 3 },
-        { title: 'Top Fuvking App Dev Tips', body: 'lorem ipsum...', author: 'Gabriella', id: 3 },
-        { title: 'Ayyein', body: 'lorem ipsum...', author: 'Baigan', id: 3 },
+        { title: 'How to become a Fuvking Developer', body: 'lorem ipsum...', author: 'Rasputin', id: 4 },
+        { title: 'Top Fuvking App Dev Tips', body: 'lorem ipsum...', author: 'Gabriella', id: 5 },
+        { title: 'Ayyein', body: 'lorem ipsum...', author: 'Baigan', id: 6 },
     ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All The Fuvking Blogs!" />
+            <BlogList blogs={blogs} title="All The Fuvking Blogs!" handleDelete={handleDelete} />
         </div>
     );
 }

84ce0c8216655b621f9769e9e639eeebc253b32c
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-04-14 17:33:52 +0530

    init

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new Fuvking website!', body: 'lorem ipsum...', author: 'Bobbombom', id: 1 },
-        { title: 'A Fuvking welcome party!', body: 'lorem ipsum...', author: 'Bhupendra Jogi', id: 2 },
-        { title: 'Top Fuvking Web Dev Tips', body: 'lorem ipsum...', author: 'Gabriella', id: 3 },
-        { title: 'How to become a Fuvking Developer', body: 'lorem ipsum...', author: 'Rasputin', id: 4 },
-        { title: 'Top Fuvking App Dev Tips', body: 'lorem ipsum...', author: 'Gabriella', id: 5 },
-        { title: 'Ayyein', body: 'lorem ipsum...', author: 'Baigan', id: 6 },
-    ])
+    const [blogs, setBlogs] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
     }
 
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Could not open the site at the moment, please try again later.');
+                }
+                return res.json();
+            }).then(data => {
+                setBlogs(data);
+                setIsLoading(false);
+                setError(null);
+            }).catch(err => {
+                setError(err.message);
+                setIsLoading(false);
+            })
+
+    }, []);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All The Fuvking Blogs!" handleDelete={handleDelete} />
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All The Fuvking Blogs!" handleDelete={handleDelete} />}
         </div>
     );
 }
 
 export default Home;
+
+
+
+
+

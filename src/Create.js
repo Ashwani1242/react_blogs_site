fa87db023803f4496cacf08212a46e2e1445b09b
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-06-08 17:56:22 +0530

    Changes Committed on 2024-06-08, 17:56

--- src/Create.js
+++ src/Create.js
@@ -1,11 +1,48 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [ title, setTitle ] = useState('');
+    const [ subTitle, setSubTitle ] = useState('');
+    const [ body, setBody ] = useState('');
+    const [ author, setAuthor ] = useState('');
+
     return ( 
         <div className="create">
             <h2>
                 Add a new Blog.
             </h2>
+            <form>
+                <label> Blog Title </label>
+                <input 
+                    type="text" 
+                    required
+                    value = { title }
+                    onChange = { (e) => setTitle( e.target.value ) }
+                />
+                <label> Blog Subtitle </label>
+                <input 
+                    type="text" 
+                    required
+                    value = { subTitle }
+                    onChange = { (e) => setSubTitle( e.target.value )} 
+                />
+                <label> Blog Body </label>
+                <textarea 
+                    required 
+                    value = { body }
+                    onChange = { (e) => setBody( e.target.value ) }
+                />
+                <label> Blog Author </label>
+                <input 
+                    required 
+                    value = { author }
+                    onChange = { (e) => setAuthor( e.target.value ) }
+                />
+                <button> Add Blog</button>
+            </form>
         </div>
      );
 }
- 
+
 export default Create;

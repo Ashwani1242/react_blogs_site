d668392d3dbe020b6afe2ae2b929511af257d06c
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-06-08 18:36:43 +0530

    Changes Committed on 2024-06-08, 18:36

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { id } = useParams()
     const { data: blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id);
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id , {
+            method: 'DELETE',
+        }).then((
+            navigate('/')
+        ))
+    }
 
     return ( <div className="blog-details">
         { isLoading && <div> Loading.... </div>}
@@ -16,7 +25,8 @@ const BlogDetails = () => {
                 <h3> { blog.subtitle } </h3>
                 <h5> written by -  { blog.author }</h5>
                 <br />
-                <p dangerouslySetInnerHTML={{ __html: blog.body.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>').replace(/\n/g, '<br>') }}></p>
+                <p dangerouslySetInnerHTML={{ __html: blog.body.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>').replace(/\n/g, '<br>') }} />
+                <button onClick = { handleClick }> Delete Blog </button>
             </article>
         )}
     </div> );

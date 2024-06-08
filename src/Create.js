4ec3b1af8b36e4bdd903b8e7aba99442bd0efd67
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-06-08 18:17:56 +0530

    Changes Committed on 2024-06-08, 18:17

--- src/Create.js
+++ src/Create.js
@@ -2,54 +2,67 @@ import { useState } from "react";
 
 const Create = () => {
 
-    const [ title, setTitle ] = useState('');
-    const [ subTitle, setSubTitle ] = useState('');
-    const [ body, setBody ] = useState('');
-    const [ author, setAuthor ] = useState('');
+    const [title, setTitle] = useState('');
+    const [subtitle, setSubtitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const blog = { title, subTitle, author , body };
+        const blog = { title, subtitle, author, body };
 
-        console.log(blog);
+        setIsLoading(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog),
+        }).then(() => {
+            setIsLoading(false);
+        })
     }
 
-    return ( 
+    return (
         <div className="create">
             <h2>
                 Add a new Blog.
             </h2>
-            <form onSubmit = { handleSubmit }>
+            <form onSubmit={handleSubmit}>
                 <label> Blog Title </label>
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     required
-                    value = { title }
-                    onChange = { (e) => setTitle( e.target.value ) }
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label> Blog Subtitle </label>
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     required
-                    value = { subTitle }
-                    onChange = { (e) => setSubTitle( e.target.value )} 
+                    value={subtitle}
+                    onChange={(e) => setSubtitle(e.target.value)}
                 />
                 <label> Blog Body </label>
-                <textarea 
-                    required 
-                    value = { body }
-                    onChange = { (e) => setBody( e.target.value ) }
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 />
                 <label> Blog Author </label>
-                <input 
-                    required 
-                    value = { author }
-                    onChange = { (e) => setAuthor( e.target.value ) }
+                <input
+                    required
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
                 />
-                <button> Add Blog</button>
+                <button disabled={isLoading}>
+                    {isLoading ? 'Adding Blog...' : 'Add Blog'}
+                </button>
+
             </form>
         </div>
-     );
+    );
 }
 
 export default Create;
